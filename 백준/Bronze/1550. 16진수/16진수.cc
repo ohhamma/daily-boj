f09a637b74b912bf5033521992007a657d95711e
YoonJu Ham <81293930+ohhamma@users.noreply.github.com>
2022-03-28 01:40:40 +0900

    [Bronze V] Title: 16진수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+using namespace std;
+
+int N;
+
+int main() {
+	FASTIO;
+	cin >> hex >> N;
+	cout << dec << N;
+}
