7b30eaa0c5a349577747d186a137c257fbaf758f
YoonJu Ham <81293930+ohhamma@users.noreply.github.com>
2022-04-02 19:20:09 +0900

    [Bronze V] Title: Next in line, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+using namespace std;
+typedef long long ll;
+
+int main() {
+  FASTIO;
+	int Y,M; cin >> Y >> M;
+	cout << 2*M-Y;
+}
