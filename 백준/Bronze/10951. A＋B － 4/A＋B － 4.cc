90bf4d54fb56aa3e42e186c0ef0380110e12a9d6
YoonJu Ham <81293930+ohhamma@users.noreply.github.com>
2022-04-03 22:11:27 +0900

    [Bronze III] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+using namespace std;
+typedef long long ll;
+
+int main() {
+  FASTIO;
+  int A,B;
+  while(cin >> A >> B)
+    cout << A+B << '\n';
+}
