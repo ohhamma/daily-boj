daf9d11f7029cb4462e0d066da431804d38b720d
YoonJu Ham <81293930+ohhamma@users.noreply.github.com>
2022-04-06 00:54:32 +0900

    [Silver V] Title: 단어 정렬, Time: 56 ms, Memory: 7464 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.cc
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -1,28 +1,30 @@
 #include <iostream>
-#include <algorithm>
 #include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
 using namespace std;
+typedef long long ll;
 
-string words[20202];		// 배열 넉넉하게 잡기
-							// 함수 내에서 너무 많은 메모리 사용하지 않기
+int N;
+string tmp;
+set<string> s;
+vector<string> v;
 
-bool cmp(string s1, string s2) {
-	if (s1.length() == s2.length()) {
-		return s1 < s2;
-	}
-	return s1.length() < s2.length();
+bool cmp(string a, string b) {
+  if(a.length()==b.length()) return a<b;
+  else return a.length()<b.length();
 }
 
 int main() {
-	int N;
-	cin >> N;
-	for (int i = 0; i < N; i++) {
-		cin >> words[i];
-	}
-	sort(words, words + N, cmp);
-
-	for (int i = 0; i < N; i++) {
-		if (words[i] != words[i + 1])
-			cout << words[i] << '\n';
-	}
+  FASTIO;
+  cin >> N;
+  for(int i=0; i<N; i++) {
+    cin >> tmp;
+    s.insert(tmp);
+  }
+  for(auto i:s) v.push_back(i);
+  sort(v.begin(),v.end(),cmp);
+  for(auto i:v) cout << i << '\n';
 }
