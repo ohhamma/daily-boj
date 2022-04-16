cf50cbabb9423b4ce0cd55a17f9ab593141574b5
YoonJu Ham <81293930+ohhamma@users.noreply.github.com>
2022-04-17 00:47:08 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+using namespace std;
+typedef long long ll;
+
+int main() {
+  FASTIO;
+  int x; cin >> x;
+  cout << x*x*x;
+}
