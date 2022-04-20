4cc1d531ab1808812aecc2d351b7340cacd6a362
YoonJu Ham <81293930+ohhamma@users.noreply.github.com>
2022-04-21 03:13:03 +0900

    [Bronze IV] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+using namespace std;
+typedef long long ll;
+
+int score,S,T;
+
+int main() {
+  FASTIO;
+  int N; cin >> N;
+  if(N) cout << "Leading the Way to the Future";
+  else cout << "YONSEI";
+}
