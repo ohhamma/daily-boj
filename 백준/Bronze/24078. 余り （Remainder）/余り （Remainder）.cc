efaf185e8c94ed654bde680c2153a24db84f44e7
YoonJu Ham <81293930+ohhamma@users.noreply.github.com>
2022-04-28 00:56:10 +0900

    [Bronze V] Title: 余り (Remainder), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+
+int main() {
+  FASTIO;
+  int X; cin >> X;
+  cout << X % 21;
+}
