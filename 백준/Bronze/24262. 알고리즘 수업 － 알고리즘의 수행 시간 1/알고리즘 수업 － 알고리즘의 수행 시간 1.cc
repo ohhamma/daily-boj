5cb84661d2ede8ade8cfca5d1f4777fa8414fb38
YoonJu Ham <81293930+ohhamma@users.noreply.github.com>
2022-04-30 01:04:30 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+using namespace std;
+typedef long long ll;
+
+int main() {
+  FASTIO;
+  int n; cin >> n;
+  cout << 1 << '\n' << 0;
+}
