06866c46e0cabdb2b443cb0f42dc782d7a304a8b
YoonJu Ham <81293930+ohhamma@users.noreply.github.com>
2022-05-07 23:47:10 +0900

    [Bronze III] Title: 피보나치 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+using namespace std;
+typedef long long ll;
+
+int n;
+ll arr[50];
+
+ll sol(int n) {
+  if(n==0||n==1) return arr[n]=n;
+  if(!arr[n]) arr[n] = sol(n-1)+sol(n-2);
+  return arr[n];
+}
+
+int main() {
+  FASTIO;
+  cin >> n;
+  cout << sol(n);
+}
