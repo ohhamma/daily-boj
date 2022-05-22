36a811c475e3513e6755a17af3653d3f597e7a87
YoonJu Ham <81293930+ohhamma@users.noreply.github.com>
2022-05-23 00:15:39 +0900

    [Bronze IV] Title: FA, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14935. FA/FA.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+using namespace std;
+typedef long long ll;
+
+string s;
+
+int main() {
+  FASTIO;
+  cin >> s;
+  cout << "FA";
+}
+
+// 항상 FA수임 (일의자리수일때)
