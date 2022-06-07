2f3ed7f1d9fe75e263a9ef4f97f53d89dbd15127
YoonJu Ham <81293930+ohhamma@users.noreply.github.com>
2022-06-08 03:45:50 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+using namespace std;
+typedef long long ll;
+
+string s;
+
+int main() {
+  FASTIO;
+  cin >> s;
+  for(char c:s) {
+    if(isupper(c)) c = tolower(c);
+    else c = toupper(c);
+    cout << c;
+  }
+}
