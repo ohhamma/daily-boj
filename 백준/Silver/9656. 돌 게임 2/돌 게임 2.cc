55b3b4d4640490e0ae15185f021a8eacc154e80b
YoonJu Ham <81293930+ohhamma@users.noreply.github.com>
2022-06-20 16:43:47 +0700

    [Silver V] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+using namespace std;
+typedef long long ll;
+
+int N;
+
+int main() {
+  FASTIO;
+  cin >> N;
+  if(N%2==1) cout << "CY";
+  else cout << "SK";
+}
